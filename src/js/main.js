81f54b4ac45621432e79a53b9bc1097dab9cdcf4
Pavel Kogut <kogut.kogut1@mail.ru>
2022-04-21 21:21:32 +0500

    Replacing images on hover

--- src/js/main.js
+++ src/js/main.js
@@ -5,6 +5,7 @@ import mask from "./modules/mask";
 import showMoreStyles from "./modules/showMoreStyles";
 import calc from "./modules/calc";
 import filter from "./modules/filter";
+import pictureSize from "./modules/pictureSize";
 
 window.addEventListener('DOMContentLoaded', () => {
     'use strict';
@@ -19,4 +20,5 @@ window.addEventListener('DOMContentLoaded', () => {
     showMoreStyles('#styles .row', '.button-styles');
     calc('#size', '#material', '#option', '.promocode', '.calc-price', '[data-calc]', formState);
     filter();
+    pictureSize('.sizes-block');
 });
