84ac6a127fb17b176cbcab863c5645bf374b177e
Pavel Kogut <kogut.kogut1@mail.ru>
2022-04-22 16:06:03 +0500

    accordion is ready

--- src/js/main.js
+++ src/js/main.js
@@ -6,6 +6,7 @@ import showMoreStyles from "./modules/showMoreStyles";
 import calc from "./modules/calc";
 import filter from "./modules/filter";
 import pictureSize from "./modules/pictureSize";
+import accordion from "./modules/accordion";
 
 window.addEventListener('DOMContentLoaded', () => {
     'use strict';
@@ -21,4 +22,5 @@ window.addEventListener('DOMContentLoaded', () => {
     calc('#size', '#material', '#option', '.promocode', '.calc-price', '[data-calc]', formState);
     filter();
     pictureSize('.sizes-block');
+    accordion('.accordion-heading');
 });
